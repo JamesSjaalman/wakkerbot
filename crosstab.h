9e8ef0cfee5f648fb9639f250f2daaf58dacca30
jamessjaalman <jamessjaalman@gmail.com>
2010-12-08 22:00:19 +0100

    crosstab2.h Antedated: 2010-12-08 22:00:19 +0100

--- crosstab.h
+++ crosstab.h
@@ -1,32 +1,51 @@
 
 
-#define CROSS_DICT_SIZE 6
-#define IDX_NIL ((unsigned)-1)
+#define IDX_NIL ((unsigned int)-1)
+#define WANT_LRU 0
+#define WANT_RESIZE 1
 
-struct crosscell {
-	unsigned sum;
+struct rowstats {
+	unsigned int uniq;
+	unsigned int sum;
+	};
+struct rowhash {
+	unsigned int head;
+	unsigned int link;
+	unsigned int key;
+	};
+struct rowlru {
+	unsigned int older;
+	unsigned int newer;
 	};
 
 struct crossrow {
-	unsigned uniq;
-	unsigned sum;
+	struct rowhash hash;
+#if WANT_LRU
+	struct rowlru lru;
+#endif
+	struct rowstats stats;
 	};
-struct crosshash {
-	unsigned head;
-	unsigned link;
-	unsigned key;
-	};
+
 struct crosstab {
-	unsigned msize;
-	unsigned freelist;
-	struct	crossrow total;
-	struct crosshash *table;
-	struct crossrow *edge;
-	struct crosscell *cells;
+	unsigned int msize;
+	unsigned int freelist;
+#if WANT_LRU
+	struct	{
+		unsigned int oldest;
+		unsigned int newest;
+		} lru;
+#endif
+	struct rowstats total;
+	double score;
+	unsigned int *index;
+	double *scores;
+	struct crossrow *table;
+	unsigned int *matrix;
 	};
 
 void crosstab_show(FILE *fp, struct crosstab *ptr );
-struct crosstab *crosstab_init(struct crosstab *old,  unsigned newsize);
-void crosstab_add_pair(struct crosstab *ptr,  unsigned one, unsigned two);
+struct crosstab *crosstab_init(unsigned int newsize);
+void crosstab_resize(struct crosstab * ptr, unsigned int newsize);
+void crosstab_free(struct crosstab *ptr);
+void crosstab_add_pair(struct crosstab *ptr,  unsigned int one, unsigned int two);
 void crosstab_print(FILE *fp, struct crosstab *ptr );
-
