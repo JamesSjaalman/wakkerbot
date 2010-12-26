30f220cda634acb8e05f30c89d3335798cb2492a
jamessjaalman <jamessjaalman@gmail.com>
2010-12-26 12:32:31 +0100

    crosstab3d.h Antedated: 2010-12-26 12:32:31 +0100

--- crosstab.h
+++ crosstab.h
@@ -47,10 +47,11 @@ struct crosstab *crosstab_init(unsigned int newsize);
 /* z */ void crosstab_reduce(struct crosstab * ptr, unsigned int newsize);
 /* s */ void crosstab_repack(struct crosstab * ptr);
 void crosstab_free(struct crosstab *ptr);
-/* x y */ void crosstab_add_pair(struct crosstab *ptr,  unsigned int one, unsigned int two);
+/* x y */ void crosstab_add_pair(struct crosstab *ptr,  unsigned int one, unsigned int two, unsigned val);
 /* p */ void crosstab_print(FILE *fp, struct crosstab *ptr );
 /* P */ void crosstab_pretty_print(FILE *fp, struct crosstab *ptr, size_t (*cnv)(char *buff, unsigned sym) );
 /* . */ void crosstab_show(FILE *fp, struct crosstab *ptr, size_t (*cnv)(char *buff, unsigned sym) );
 /* ? */ double crosstab_ask(struct crosstab * ptr, unsigned sym);
 /* g */ unsigned crosstab_get(struct crosstab * ptr, unsigned idx);
+/* l */ unsigned crosstab_bin_load(struct crosstab *ptr, char *name);
 
