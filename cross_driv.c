6ea935e9e5441582c66527387ec90cb464cb91bd
jamessjaalman <jamessjaalman@gmail.com>
2011-02-02 17:02:33 +0100

    cross_driv4.c Antedated: 2011-02-02 17:02:33 +0100

--- cross_driv.c
+++ cross_driv.c
@@ -4,9 +4,10 @@
 
 #include "crosstab.h"
 
+static size_t format_num(char *buff, unsigned int num);
 static size_t format_num(char *buff, unsigned int num)
 {
-return sprintf(buff, "%05u", num);
+return sprintf(buff, "%5u", num);
 }
 
 int main()
@@ -31,17 +32,18 @@ while (fgets(buff, sizeof buff, stdin)) {
 		rc = sscanf( cp, "%u", &one);
 		if (rc != 1) continue;
 		crosstab_reduce( cdp, one); continue;
-	case 'r': 
-		cp += strcspn(cp, " \t\n");
-		rc = sscanf( cp, "%u", &one);
-		if (rc != 1) continue;
-		crosstab_reduce( cdp, one); continue;
 	case 'l': 
 		cp += strcspn(cp, " \t\n");
 		cp += strspn(cp, " \t\n");
 		one = strcspn(cp, " \t\n");
 		cp[one] = 0;
 		crosstab_bin_load( cdp, cp); continue;
+	case 'n': 
+		cp += strcspn(cp, " \t\n");
+		cp += strspn(cp, " \t\n");
+		one = strcspn(cp, " \t\n");
+		cp[one] = 0;
+		crosstab_labels_load( cdp, cp); continue;
 	case 's': 
 		crosstab_repack( cdp); continue;
 	default:
@@ -51,7 +53,8 @@ while (fgets(buff, sizeof buff, stdin)) {
 		crosstab_add_pair( cdp, one, two, 1);
 	case 'p': crosstab_print(stderr, cdp); continue;
 	case 'P': crosstab_pretty_print(stderr, cdp, format_num); continue;
-	case 'd': crosstab_show(stderr, cdp, format_num); continue;
+	/* case 'd': crosstab_show(stdout, cdp, format_num); continue; */
+	case 'd': crosstab_show(stdout, cdp, NULL); continue;
 	case '?': 
 		cp += strcspn(cp, " \t\n");
 		rc = sscanf( cp, "%u", &one);
@@ -62,9 +65,9 @@ while (fgets(buff, sizeof buff, stdin)) {
 	case 'q': goto quit;
 	case 'h':
 		fprintf(stderr, "i <size>\t:= Initialize\n" );
-		fprintf(stderr, "z <size>\t:= ResiZe\n" );
-		fprintf(stderr, "r <size>\t:= Reduce to size\n" );
-		fprintf(stderr, "l <name>\t:= Load file\n" );
+		fprintf(stderr, "z <size>\t:= Reduce to size\n" );
+		fprintf(stderr, "l <name>\t:= Load file with crostabs (crosstab.dmp)\n" );
+		fprintf(stderr, "n <name>\t:= Load file with names (megahal.dic)\n" );
 		fprintf(stderr, "s <void>\t:= Repack\n" );
 		fprintf(stderr, "num1 num2\t:= Insert num1,num2\n" );
 		fprintf(stderr, "p \t:= Print\n" );
