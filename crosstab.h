1c52b30b55b0ac6804806af197d9737bc8ebe7fb
jamessjaalman <jamessjaalman@gmail.com>
2012-01-19 15:31:06 +0100

    crosstab3e.h Antedated: 2012-01-19 15:31:06 +0100

--- crosstab.h
+++ crosstab.h
@@ -1,6 +1,5 @@
 
 #define IDX_NIL ((unsigned int)-1)
-#define WANT_LRU 0
 
 struct rowstats {
 	unsigned int uniq;
@@ -11,31 +10,18 @@ struct rowhash {
 	unsigned int link;
 	unsigned int key;
 	};
-struct rowlru {
-	unsigned int older;
-	unsigned int newer;
-	};
 
 struct crossrow {
 	struct rowhash hash;
-#if WANT_LRU
-	struct rowlru lru;
-#endif
 	struct rowstats payload;
 	};
 
 struct crosstab {
 	unsigned int msize;
 	unsigned int freelist;
-#if WANT_LRU
-	struct	{
-		unsigned int oldest;
-		unsigned int newest;
-		} lru;
-#endif
 	struct rowstats total;
 	double score;
-	char meuk[22];
+	/* char meuk[22]; */
 	unsigned int *index;
 	double *scores;
 	struct crossrow *table;
@@ -49,9 +35,12 @@ struct crosstab *crosstab_init(unsigned int newsize);
 void crosstab_free(struct crosstab *ptr);
 /* x y */ void crosstab_add_pair(struct crosstab *ptr,  unsigned int one, unsigned int two, unsigned val);
 /* p */ void crosstab_print(FILE *fp, struct crosstab *ptr );
-/* P */ void crosstab_pretty_print(FILE *fp, struct crosstab *ptr, size_t (*cnv)(char *buff, unsigned sym) );
-/* . */ void crosstab_show(FILE *fp, struct crosstab *ptr, size_t (*cnv)(char *buff, unsigned sym) );
+/* P */ void crosstab_pretty_print(FILE *fp, struct crosstab *ptr , size_t (*cnv)(char *buff, unsigned sym) );
+/* . */ void crosstab_show(FILE *fp, struct crosstab *ptr
+	, size_t (*cnv)(char *buff, unsigned sym)
+	, double (*symval)(unsigned sym) );
 /* ? */ double crosstab_ask(struct crosstab * ptr, unsigned sym);
 /* g */ unsigned crosstab_get(struct crosstab * ptr, unsigned idx);
 /* l */ unsigned crosstab_bin_load(struct crosstab *ptr, char *name);
+/* n */ unsigned crosstab_labels_load(struct crosstab *dummy, char *name);
 
