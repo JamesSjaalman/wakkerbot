8b72a9c79d6afd4c5f1b84ce5a6ac53aa15d4f2f
jamessjaalman <jamessjaalman@gmail.com>
2017-04-16 17:05:41 +0200

    crosstab.h Antedated: 2017-04-16 17:05:41 +0200

--- crosstab.h
+++ crosstab.h
@@ -1,3 +1,5 @@
+#ifndef CROSSTAB_H
+#define CROSSTAB_H 1
 
 #define IDX_NIL ((unsigned int)-1)
 
@@ -5,6 +7,7 @@ struct rowstats {
 	unsigned int uniq;
 	unsigned int sum;
 	};
+	/* hashtable, combined with array of head pointers */
 struct rowhash {
 	unsigned int head;
 	unsigned int link;
@@ -16,31 +19,50 @@ struct crossrow {
 	struct rowstats payload;
 	};
 
+	/*
+	** Since the vicinity matrix is symmetric, we need only store N*(N+1)/2 elements
+	*/
 struct crosstab {
+#if WANT_FIX
+	unsigned nfixed;
+#else
+#define nfixed 0
+#endif
 	unsigned int msize;
-	unsigned int freelist;
-	struct rowstats total;
-	double score;
+	unsigned int freelist;	/* points to LL of free cells (in table[] */
+	struct rowstats total;	/* the main score (counts) */
+	double score;		/* The main score (eigenvalue) */
 	/* char meuk[22]; */
-	unsigned int *index;
-	double *scores;
-	struct crossrow *table;
-	unsigned int *matrix;
+	unsigned int *index;	/* the numerical offsets into table[] , in descending order */
+	double *scores;		/* The resulting scores of the words */
+	struct crossrow *table;	/* hashtable+payload[msize] */
+	unsigned int *matrix;	/* msize*(msize+1)/2 crosstable */
 	};
 
 struct crosstab *crosstab_init(unsigned int newsize);
+void crosstab_free(struct crosstab *ptr);
+
+/* V---- the command-letter for cross-driv */
 /* i */ void crosstab_resize(struct crosstab * ptr, unsigned int newsize);
+
+        /* find the weakest elements and put them on the freelist */
 /* z */ void crosstab_reduce(struct crosstab * ptr, unsigned int newsize);
 /* s */ void crosstab_repack(struct crosstab * ptr);
-void crosstab_free(struct crosstab *ptr);
 /* x y */ void crosstab_add_pair(struct crosstab *ptr,  unsigned int one, unsigned int two, unsigned val);
+
+	/* ------------------------- */
 /* p */ void crosstab_print(FILE *fp, struct crosstab *ptr );
+	/* Uses callback functions to format the character content+weight of the token given its numeric value */
 /* P */ void crosstab_pretty_print(FILE *fp, struct crosstab *ptr , size_t (*cnv)(char *buff, unsigned sym) );
 /* . */ void crosstab_show(FILE *fp, struct crosstab *ptr
 	, size_t (*cnv)(char *buff, unsigned sym)
 	, double (*symval)(unsigned sym) );
+	/* get token -->> value */
 /* ? */ double crosstab_ask(struct crosstab * ptr, unsigned sym);
+	/* get index -->> value (for iteraterators */
 /* g */ unsigned crosstab_get(struct crosstab * ptr, unsigned idx);
+
 /* l */ unsigned crosstab_bin_load(struct crosstab *ptr, char *name);
 /* n */ unsigned crosstab_labels_load(struct crosstab *dummy, char *name);
 
+#endif /* CROSSTAB_H */
